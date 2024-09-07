fa2e9281afd513abad1cce8ef5342037a99ffa9e
hbhungg <hung.ba.huynh@proton.me>
2024-09-07 22:57:43 +1000

    format and comment

--- rcore/src/cpu.rs
+++ rcore/src/cpu.rs
@@ -22,35 +22,37 @@ impl CPU {
     }
   }
 
+  /// Load data into memory at an address
   pub fn load(&mut self, addr: u32, data: &[u8]) {
     let start = usize::try_from(addr - MAGIC_START).unwrap();
     let end = start + usize::try_from(data.len()).unwrap();
     self.memory[start..end].copy_from_slice(data);
   }
 
+  /// Read 32bit value from an address
   pub fn read32(&self, addr: u32) -> u32 {
     let start = usize::try_from(addr - MAGIC_START).unwrap();
     u32::from_le_bytes(self.memory[start..start + 4].try_into().unwrap())
   }
 
+  /// Pretty print coredump
   pub fn coredump(&self, size: usize, style: DumpStyle) {
     let chunk_size: usize = 16;
     for (idx, chunk) in self.memory[0..size].chunks(chunk_size).enumerate() {
-      let row = chunk
-        .chunks(4)
-        .enumerate()
-        .fold(format!("0x{:08x}: ", MAGIC_START + (idx * chunk_size) as u32), |acc, (_, byte)| {
-          let x = u32::from_le_bytes(byte.try_into().unwrap());
-          let formatted_byte = match style {
-            DumpStyle::Hex => format!("0x{:08x} ", x),
-            DumpStyle::Bin => format!("0b{:032b} ", x),
-          };
-          acc + &formatted_byte
-        });
+      let addr = MAGIC_START + (idx * chunk_size) as u32;
+      let row = chunk.chunks(4).enumerate().fold(format!("0x{:08x}: ", addr), |acc, (_, byte)| {
+        let x = u32::from_le_bytes(byte.try_into().unwrap());
+        let formatted_byte = match style {
+          DumpStyle::Hex => format!("0x{:08x} ", x),
+          DumpStyle::Bin => format!("0b{:032b} ", x),
+        };
+        acc + &formatted_byte
+      });
       println!("{}", row);
     }
   }
 
+  /// Main CPU loop
   pub fn step(&self) {
     let ins: u32 = self.read32(self.register[32]);
   }
