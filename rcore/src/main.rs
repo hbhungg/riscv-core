8af0a99c21decdcd56e4586a956b7e3f8a864536
hbhungg <hung.ba.huynh@proton.me>
2024-09-07 21:48:59 +1000

    read32 return u32 (not [u8; 4] lol) and mem_dump kinda works, somehow...

--- rcore/src/main.rs
+++ rcore/src/main.rs
@@ -5,47 +5,62 @@ use glob::glob;
 use elf::endian::LittleEndian;
 use elf::ElfBytes;
 
+// 1MB at 0x80000000
+const MEM_SIZE: usize = 0x40000;
 struct CPU {
-  memory: Vec<u8>,
+  memory: [u8; MEM_SIZE],
   register: [u32; 33],
 }
 
+enum DumpStyle {
+  Hex,
+  Bin,
+}
+
 impl CPU {
-  const MAGIC_START: u64 = 0x80000000;
+  const MAGIC_START: u32 = 0x80000000;
+  // Init all 0s
   fn new() -> Self {
     Self {
-      // 1MB at 0x80000000
-      memory: vec![0; 0x40000],
+      memory: [0; MEM_SIZE],
       register: [0; 33],
     }
   }
 
-  fn load(&mut self, addr: u64, data: &[u8]) {
+  fn load(&mut self, addr: u32, data: &[u8]) {
     let start = usize::try_from(addr - CPU::MAGIC_START).unwrap();
     let end = start + usize::try_from(data.len()).unwrap();
     self.memory[start..end].copy_from_slice(data);
-    // self.print_memory_chunk(start, end);
   }
 
-  fn read32(&self, addr: u64) -> [u8; 4] {
+  fn read32(&self, addr: u32) -> u32 {
     let start = usize::try_from(addr - CPU::MAGIC_START).unwrap();
-    self.memory[start..start+4].try_into().expect("should be size 4")
+    u32::from_le_bytes(self.memory[start..start + 4].try_into().unwrap())
   }
 
   fn reg_dump(&self) {
     println!("Registers: {:?}", self.register)
   }
-  fn mem_dump(&self) {
-    println!("{:?}", self.memory)
+
+  fn mem_dump(&self, size: usize, style: DumpStyle) {
+    let chunk_size: usize = 16;
+    for (idx, chunk) in self.memory[0..size].chunks(chunk_size).enumerate() {
+      let row = chunk.iter().enumerate().fold(
+        format!("{:08x}: ", idx * chunk_size),
+        |acc, (_, &byte)| {
+          let formatted_byte = match style {
+            DumpStyle::Hex => format!("{:02x} ", byte),
+            DumpStyle::Bin => format!("{:08b} ", byte),
+          };
+          acc + &formatted_byte
+        }
+      );
+      println!("{}", row);
+    }
   }
 
-  fn print_memory_chunk(&self, start: usize, end: usize) {
-    // Ensure the slice bounds are within the valid range
-    if start <= end && end <= self.memory.len() {
-      println!("{:?}", &self.memory[start..end]);
-    } else {
-      eprintln!("Error: Slice bounds are out of range.");
-    }
+  fn step(&self) {
+    let ins: u32 = self.read32(self.register[32]);
   }
 }
 
@@ -57,13 +72,14 @@ fn read_elf(path: &PathBuf) {
 
   for phdr in elf.segments().expect("Failed to get ELF segments") {
     let data = elf.segment_data(&phdr).unwrap();
-    if phdr.p_vaddr >= CPU::MAGIC_START {
-      cpu.load(phdr.p_vaddr, data);
+    let offset: u32 = u32::try_from(phdr.p_vaddr).expect("could not cast u32?");
+    if offset >= CPU::MAGIC_START {
+      cpu.load(offset, data);
     }
   }
 
   // cpu.reg_dump();
-  cpu.mem_dump();
+  cpu.mem_dump(400, DumpStyle::Hex);
 }
 
 fn main() {
