3c2d812ed8f39d5be8fcb66daf45bfd3acb89987
hbhungg <hung.ba.huynh@proton.me>
2024-09-08 00:25:46 +1000

    regdump and fn for set and get reg, so that we can hardcode reg 0 to 0, and disallow set on reg 0

--- rcore/src/cpu.rs
+++ rcore/src/cpu.rs
@@ -1,6 +1,8 @@
 // 1MB at 0x80000000
 const MEMSIZE: usize = 0x40000;
+// RISC-V have 32 register
 const REGSIZE: usize = 33;
+const PC: usize = 32;
 pub const MAGIC_START: u32 = 0x80000000;
 
 pub struct CPU {
@@ -20,7 +22,7 @@ impl CPU {
     register[32] = MAGIC_START;
     Self {
       memory: [0; MEMSIZE],
-      register: register
+      register: register,
     }
   }
 
@@ -35,6 +37,31 @@ impl CPU {
   pub fn read32(&self, addr: u32) -> u32 {
     let start = usize::try_from(addr - MAGIC_START).unwrap();
     u32::from_le_bytes(self.memory[start..start + 4].try_into().unwrap())
+  }
+
+  fn setreg(&mut self, reg: usize, val: u32) {
+    // Write to reg 0 will have no effect since it is hardwire to 0
+    if reg != 0 {
+      self.register[reg] = val;
+    }
+  }
+
+  fn getreg(&self, reg: usize) -> u32 {
+    // Just to be safe
+    match reg {
+      0 => 0,
+      _ => self.register[reg],
+    }
+  }
+
+  fn regdump(&self) {
+    // Print a header
+    println!("{:<5} {:>8}", "Reg", "Value");
+    // Print each register with its value
+    for idx in 0..31 {
+      println!("{:<5} {:08x}", format!("x{}", idx), self.register[idx])
+    }
+    println!("{:<5} {:08x}", format!("{}", "PC"), self.register[32]);
   }
 
   /// Pretty print coredump
@@ -57,9 +84,11 @@ impl CPU {
   /// Main CPU loop
   pub fn step(mut self) {
     loop {
-      let ins: u32 = self.read32(self.register[32]);
-      println!("{:08x}: {:08x}", self.register[32], ins);
-      self.register[32] = self.register[32] + 4;
+      let vpc = self.getreg(PC);
+      let ins: u32 = self.read32(vpc);
+      println!("{:08x}: {:08x}", vpc, ins);
+      self.setreg(PC, vpc + 4);
+      self.regdump();
       break;
     }
   }
